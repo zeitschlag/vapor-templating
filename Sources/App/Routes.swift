5e24e659fc3376942d529d00834b88dded9b763f
Nathan Mattes <hallo@bullenscheisse.de>
2017-06-16 12:50:29 +0200

    First Demo: Added simple hello world-template

--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -3,23 +3,7 @@ import Vapor
 extension Droplet {
     func setupRoutes() throws {
         get("hello") { req in
-            var json = JSON()
-            try json.set("hello", "world")
-            return json
+            return try self.view.make("hello")
         }
-
-        get("plaintext") { req in
-            return "Hello, world!"
-        }
-
-        // response to requests to /info domain
-        // with a description of the request
-        get("info") { req in
-            return req.description
-        }
-
-        get("description") { req in return req.description }
-        
-        try resource("posts", PostController.self)
     }
 }
