bae5050c84f16d73265e59c06fcd4796f7f47507
Nathan Mattes <hallo@bullenscheisse.de>
2017-06-16 12:58:27 +0200

    Added variable-tag-demo

--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -1,9 +1,24 @@
 import Vapor
+import Foundation
 
 extension Droplet {
     func setupRoutes() throws {
+        
         get("hello") { req in
             return try self.view.make("hello")
+        }
+        
+        // variable
+        get("time") { req in
+            
+            // Don't try this at home!
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "HH:mm:ss"
+            
+            let timeString = dateFormatter.string(from: Date())
+            
+            let context = ["time": timeString]
+            return try self.view.make("time", context)
         }
     }
 }
